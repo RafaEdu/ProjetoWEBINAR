8dd1310be9a66eec0855cecaddde5a01dcddbacb
Rafael Eduardo Dreissig <rafaeldreissig@gmail.com>
2024-09-26 21:03:39 -0300

    Tela Cadastro de Novo Curso

--- src/pages/CadastroTreinamento/index.jsx
+++ src/pages/CadastroTreinamento/index.jsx
@@ -3,8 +3,14 @@ import './style.css'
 function CadastroTreinamento() {
   return (
 
-    <div>
-      <h1>Frukizada</h1>
+    <div className='container'>
+      <form action="">
+        <h1>Cadastro De Novo Curso</h1>
+        <input name="titulo" type='text' />
+        <input name="descrição" type='text' />
+        <input name="vencimento" type='date' />
+        <input name="anexo" type='file' />
+      </form>
     </div>
 
   )
